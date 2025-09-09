0e854906e2661e2e94bf786662366e7bee70b37c
komiwalnut <62009152+komiwalnut@users.noreply.github.com>
2025-09-09 19:31:52 +0800

    Fix typo in layout.tsx by removing extraneous characters after ScrollProgress component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({
           <ColorThemeProvider />
         </Suspense>
         
-        <ScrollProgress />oes
+        <ScrollProgress />
         <FloatingElements />
         <SecretGamingProfiles />
         <KeyboardHint />
